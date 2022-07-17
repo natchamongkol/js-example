c5f02157502c067e29353d013f3db3e81e8fe704
natchamongkol <natchamongkol4321@gmail.com>
2022-07-17 16:48:22 +0700

    add-coin-cash

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-import './style.css'
+import JSConfetti from 'js-confetti'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import "./style.css";
+
+const jsConfetti = new JSConfetti()
+const btncoin = document.getElementById("btn-coin");
+const btncash = document.getElementById("btn-cash");
+const walletElement = document.getElementById("wallet");
+
+const coiniconurl = "https://cdn.icon-icons.com/icons2/2104/PNG/512/coin_icon_129365.png";
+const cashiconurl = "https://cdn.icon-icons.com/icons2/550/PNG/512/business-color_money-coins_icon-icons.com_53446.png";
+
+const min = Math.ceil(300);
+const max = Math.floor(1500);
+const targetRich = Math.floor(Math.random() * (max - min) + min);
+console.log(`targetRich = ${targetRich}`);
+
+function updateincome(value) {
+  const incomeElement = document.getElementById('income')
+  const currentValue = parseInt(incomeElement.textContent);
+
+  let newValue = currentValue + value;
+  incomeElement.textContent = newValue;
+  if(newValue > targetRich) {
+    jsConfetti.addConfetti()
+  }
+}
+
+btncoin.addEventListener("click", function() {
+  let iconElementHtml = `<img src="${coiniconurl}" />`
+  walletElement.insertAdjacentHTML('beforeend', iconElementHtml);
+
+  updateincome(10);
+});
+
+btncash.addEventListener("click", function() {
+  let iconElementHtml = `<img src="${cashiconurl}" />`
+  walletElement.insertAdjacentHTML('beforeend', iconElementHtml);
+
+  updateincome(100);
+});
